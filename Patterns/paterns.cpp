bb83c698bf62ee344813eb5c92c492f8c6d153fa
bhakarboy01 <hemantbhakar21@gmail.com>
2025-08-01 20:53:08 +0530

    Patterns 1

---
+++ Patterns/paterns.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+// 1st pattern
+    // int n = 5;
+    // for (int i=0; i<n; i++){
+    //     for(int j = 0; j<n; j++){
+    //         cout << "*";
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< endl<<endl<<endl;
+
+// 2nd pattern
+    // int n = 5;
+    // for (int i=1; i<=n; i++){
+    //     for(int j = 0; j<i; j++){
+    //         cout << "*";
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< endl<<endl<<endl;
+
+// 3rd pattern
+    // int n = 5;
+    // for (int i=1; i<=5; i++){
+    //     for(int j = 1; j<=i; j++){
+    //         cout << i;
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< endl<<endl<<endl;
+
+// 4th pattern
+    // int n = 5;
+    // for (int i=1; i<=5; i++){
+    //     for(int j = 1; j<=i; j++){
+    //         cout << j;
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< endl<<endl<<endl;
+
+// 5th pattern
+    // int n = 5;
+    // for (int i=n; i>0; i--){
+    //     for(int j=0; j<i; j++){
+    //         cout << "*";
+    //     }
+    //     cout<< endl;
+    // }
+    // cout<< endl<<endl<<endl;
+
+// 6th pattern
+    int n = 5;
+    for (int i=n; i>0; i--){
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+        cout<< endl;
+    }
+    // cout<< endl<<endl<<endl;
+
+
+    
+}
